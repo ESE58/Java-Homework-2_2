e2b6dc0c50bce422070c7fdafb1004ca77218dc6
ESE58 <shepshop@ya.ru>
2021-10-08 22:28:56 +0300

    Code formatted

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         int amountInitial = 200;
-        int payment = 1300;
+        int payment = 1200;
         int countValue = 100;
         int countBonus = payment / countValue;
         int bonusValue = 1;
         int bonus;
 
-            if (payment > 1000) {
-        bonus = countBonus * bonusValue;
+        if (payment > 1000) {
+            bonus = countBonus * bonusValue;
         } else {
             bonus = 0;
         }
